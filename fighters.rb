7e6b5b9c97fc30bb46def77511d52307aa0e522f
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-02-14 22:27:19 +0000

    Completed defended? and conflict_free? methods

--- fighters.rb
+++ fighters.rb
@@ -1,13 +1,52 @@
+require 'ap'
+require 'pp'
+
 class Fighters
     attr_reader :cowboys
     attr_reader :attacks
 
     def initialize(cowboys, attacks)
         @cowboys = cowboys
-        @attacks = attacks
+        # This line uses functional programming to group all attacks by attacker, and make it a hash 
+        @attacks = Hash[ attacks.group_by(&:first).map{ |k,a| [k,a.map(&:last)] } ]
     end
 
     def to_s
         "Cowboys: #{@cowboys} | Attacks: #{@attacks}"
     end
+
+    def conflict_free?(group)
+    	# Get the list of cowboys attacked by this group
+    	targets = @attacks.values_at(*group)
+    	# Make it pretty
+    	targets.flatten!.uniq!
+
+   		if (targets & group).empty?
+   			# No intersection between the cowboys this group is shooting at, and the cowboys in this group
+   			return true
+   		else
+   			return false
+	    end
+    end
+
+    # true if a member of group is attacking any of the cowboys attacking cowboy
+    def defended?(cowboy, group)
+    	# find the group's targets (1) - 
+    	# find the attackers of the cowboy (2)
+    	# return true if the intersection of 1 and 2 is not empty
+
+    	# from the attacks hash, we need each key whose value contains the cowboy
+
+    	cowboy_attacks = attacks.select{ |attacker,targets| targets.include? cowboy}
+    	cowboy_attackers = cowboy_attacks.keys
+    	group_targets = @attacks.values_at(*group).flatten.uniq.compact
+
+    	if (cowboy_attackers & group_targets).empty?
+    		return false
+    	else
+    		return true
+    	end
+
+    end
+
 end
--- fighters_test.rb
+++ fighters_test.rb
@@ -2,23 +2,45 @@ require './fighters.rb'
 require 'rspec'
 require 'ap'
 
-
-describe "A gunfight" do
+describe "A gunfight should" do    
 
     before(:all) do
-        @cowboys = [:a,:b,:c]
-        @attacks = [[:a,:b],[:b,:c],[:c,:a],[:a,:c]]
+        @cowboys = [:a,:b,:c,:d,:e]
+        @attacks = [[:a,:d],[:b,:a],[:b,:c],[:b,:d],[:d,:b],[:e,:b]]
+        @attacks_hash = {
+            :a => [:d],
+            :b => [:a,:c,:d],
+            :d => [:b],
+            :e => [:b]
+        }
         @gunfight= Fighters.new @cowboys,@attacks
-        ap @gunfight
+
     end
 
-    it "should have a populated list of cowboys" do
-        @gunfight.cowboys.should =~ @cowboys
+    it "have a populated list of cowboys" do
+        @gunfight.cowboys.should == @cowboys
     end
 
-    it "should have a populated list of attacks" do
-        @gunfight.attacks.should =~ @attacks
+    it "create a hash listing attacks" do
+        @gunfight.attacks.should == @attacks_hash
     end
+
+    it "determine if a given set of cowboys is conflict free" do
+        @gunfight.conflict_free?([:a,:c]).should be_true
+        @gunfight.conflict_free?([:b,:c]).should be_false
+    end
+
+    it "determine if a cowboy is defended by a given group" do
+        @gunfight.defended?(:b,[:a,:c]).should be_true
+        @gunfight.defended?(:e,[:a,:c]).should be_false
+    end
+
+    it "determine if a given group of cowboys is self-defending" do
+        @gunfight.self_defended?([:a,:e]).should be_true
+        @gunfight.self_defended?([:d,:e]).should be_false
+    end
+
 
 end
+
 
